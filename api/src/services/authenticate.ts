160cc9f22df481ec2c9de222a5f0fd8ed44323a2
AbraaoDev <abraaohomualdo@gmail.com>
2025-07-30 09:11:55 -0300

    update: service authenticate

---
+++ api/src/services/authenticate.ts
@@ -0,0 +1,27 @@
+import { compare } from 'bcryptjs';
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository';
+import type { AuthenticateBody } from '@/schemas/authenticate';
+import { InvalidCredentialsError } from './errors/invalid-credentials-error';
+
+export async function authenticateService({
+  email,
+  password,
+}: AuthenticateBody) {
+  const prismaUsersRepository = new PrismaUsersRepository();
+
+  const user = await prismaUsersRepository.findByEmail(email);
+
+  if (!user) {
+    throw new InvalidCredentialsError();
+  }
+
+  const doesPasswordMatches = await compare(password, user.passwordHash);
+
+  if (!doesPasswordMatches) {
+    throw new InvalidCredentialsError();
+  }
+
+  return {
+    user,
+  };
+}
